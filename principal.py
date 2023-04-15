0a9ba510d246eb15f947040f63276b7d2a3510bd
CorsairRom <ric.romero@duocuc.cl>
2023-04-15 05:28:16 -0400

    prueba

--- principal.py
+++ principal.py
@@ -2,4 +2,4 @@ import scrappingCatalogMegazipByUrl
 
 print("hola Mundo")
 
-scrappingCatalogMegazipByUrl.data_bike_by_url('https://www.megazip.net/zapchasti-dlya-motocyklov/kawasaki/zx600-16026/zx600-31577/zx600-a1-683991')
+scrappingCatalogMegazipByUrl.data_bike_by_url('https://www.megazip.net/zapchasti-dlya-motocyklov/kawasaki/zx1100-16030/zx1100-31581/zx1100-c1-805894')
