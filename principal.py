005e13844820e2e09aced392f02f2a48a58fa094
CorsairRom <ric.romero@duocuc.cl>
2023-04-15 05:22:16 -0400

    checking other manufacturers

--- principal.py
+++ principal.py
@@ -2,4 +2,4 @@ import scrappingCatalogMegazipByUrl
 
 print("hola Mundo")
 
-scrappingCatalogMegazipByUrl.data_bike_by_url('https://www.megazip.net/zapchasti-dlya-motocyklov/suzuki/gsx-r750-2301/gsx-r750-13989/gsx-r750u2k8-e2-k8-768043')
+scrappingCatalogMegazipByUrl.data_bike_by_url('https://www.megazip.net/zapchasti-dlya-motocyklov/kawasaki/zx600-16026/zx600-31577/zx600-a1-683991')
